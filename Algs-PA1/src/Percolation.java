6f251f79f74f5756c76d4c4a0dd429fe98b2be24
ABAmvrosov <abamvrosov@gmail.com>
2015-09-22 14:46:04 +0300

    Last ver PA1 PA2
    PA1 score = 92,5 \ 100?
    PA2 score = 100 w\o bonus

--- Algs-PA1/src/Percolation.java
+++ Algs-PA1/src/Percolation.java
@@ -1,74 +1,66 @@
-import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdOut;
-import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
-    private final boolean OPEN = true;
-    private final boolean BLOCKED = false;
-    private boolean[] grid;
-    private int N;
-    private WeightedQuickUnionUF UF;
+    private boolean[][] grid;
+    private WeightedQuickUnionUF uf;
+
     // create N-by-N grid, with all sites blocked
-    public Percolation(int N) throws java.lang.IllegalArgumentException {
-        if ( N <= 0 )
+    public Percolation(int N) {
+        if (N <= 0)
             throw new java.lang.IllegalArgumentException("N must be > 0");
-        this.N=N;
-        grid = new boolean[N*N+1];
-        UF = new WeightedQuickUnionUF(N*N+1);
-        for (int cell=1; cell<=N*N; cell++) {grid[cell] = BLOCKED;}
-    };
-    // validating i,j
-    private void validate(int i,int j) throws java.lang.IndexOutOfBoundsException {
-        if ( (i < 1) || (i > N) )
-            throw new java.lang.IndexOutOfBoundsException("There is no such CELL("+i+","+j+") in given GRID("+N+","+N+")");
-        if ( (j < 1) || (j > N) )
-            throw new java.lang.IndexOutOfBoundsException("There is no such CELL("+i+","+j+") in given GRID("+N+","+N+")");
+        grid = new boolean[N][N];
+        uf = new WeightedQuickUnionUF(N*N+2);
+        for (int i = 0; i <= N; i++) { uf.union(0, i); }               // top-site
+        for (int i = N*N-N+1; i <= N*N+1; i++) { uf.union(N*N+1, i); } // bottom-site
     }
+
+    // validating i,j
+    private void validate(int i, int j) {
+        if ((i < 1) || (i > grid.length))
+            throw new java.lang.IndexOutOfBoundsException("There is no such CELL");
+        if ((j < 1) || (j > grid.length))
+            throw new java.lang.IndexOutOfBoundsException("There is no such CELL");
+    }
+
     //Coordinates i,j converting to massive index
     private int index(int i, int j) {
-        return ((i - 1) * N + j);
+        return ((i - 1) * grid.length + j);
     }
+
     // open site (row i, column j) if it is not open already
-    public void open(int i, int j)  {
-        validate(i,j);
-        if (grid[index(i,j)] == OPEN) return;
-        else {
-            grid[index(i,j)] = OPEN;
-            if ( (i-1>0) && isOpen(i-1,j)) {UF.union(index(i,j),index(i-1,j));}   // union for upper cell
-            if ( (i+1<=N) && isOpen(i+1,j)) {UF.union(index(i,j),index(i+1,j));}  // union for bottom cell
-            if ( (j-1>0) && isOpen(i,j-1)) {UF.union(index(i,j),index(i,j-1));}   // union for left cell
-            if ( (j+1<=N) && isOpen(i,j+1)) {UF.union(index(i,j),index(i,j+1));}  // union for right cell
+    public void open(int i, int j) {
+        validate(i, j);
+        if (!grid[i-1][j-1]) {
+            grid[i-1][j-1] = true;
+            if ((i > 1) && isOpen(i-1, j)) { uf.union(index(i, j), index(i-1, j)); }            // union for upper cell
+            if ((i < grid.length) && isOpen(i+1, j)) { uf.union(index(i, j), index(i+1, j)); }  // union for bottom cell
+            if ((j > 1) && isOpen(i, j-1)) { uf.union(index(i, j), index(i, j-1)); }            // union for left cell
+            if ((j < grid.length) && isOpen(i, j+1)) { uf.union(index(i, j), index(i, j+1)); }  // union for right cell
         }
     };
+
     // is site (row i, column j) open?
     public boolean isOpen(int i, int j) {
-        validate(i,j);
-        return grid[index(i,j)];
+        validate(i, j);
+        return grid[i-1][j-1];
     };
 
     // is site (row i, column j) full?
     public boolean isFull(int i, int j) {
-        validate(i,j);
-        if (isOpen(i,j) && i == 1 && j<=N) return true;
-        if (isOpen(i,j)) {
-            for (int topsite=1; topsite<=N; topsite++) {
-                if (UF.connected(index(i, j), topsite)) return true;
-            }
-            return false;
-        }
-        else return false;
-    };
+        validate(i, j);
+        return isOpen(i, j) && uf.connected(index(i, j), 0);
+    }
 
     // does the system percolate?
-    public boolean percolates(){
-        for (int k=1;k<=N;k++) {
-            if (isOpen(1, k)) {
-                for (int bottomsite = N * N - N + 1; bottomsite <= N * N; bottomsite++) {
-                    if (UF.connected(index(1, k), bottomsite)) return true;
-                }
-            }
-        }
-        return false;
+    public boolean percolates() {
+        if (grid.length == 1) { return isOpen(1,1); }
+        else { return uf.connected(0, grid.length*grid.length-1); }
+    }
+    public static void main(String[] args) {
+        Percolation perc = new Percolation(10);
+        perc.open(1, 1);
+        perc.open(1, 2);
+        StdOut.print(perc.uf.connected(1, 2));
     }
 }
--- Algs-PA1/src/PercolationStats.java
+++ Algs-PA1/src/PercolationStats.java
@@ -2,29 +2,28 @@ import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
 
 public class PercolationStats {
-    private Percolation grid;
-    private int N,T,count;
-    private double mean;
-    private double stddev;
-    private double confidenceLo, confidenceHi;
+    private int T;
     private double[] counts;
     // perform T independent experiments on an N-by-N grid
     public PercolationStats(int N, int T) {
-        this.T=T; this.N=N;
+        if (T <= 0) { throw new java.lang.IllegalArgumentException(); }
+        if (N <= 0) { throw new java.lang.IllegalArgumentException(); }
+        this.T = T;
+        Percolation grid;
         counts = new double[T];
-        int a = 0,i,j;
-        for (int ExpNum=0; ExpNum < T; ExpNum++) {
+        int a = 0, i, j;
+        for (int ExpNum = 0; ExpNum < T; ExpNum++) {
             grid = new Percolation(N);
-            count=0;
-            while(!grid.percolates()) {
-                i = StdRandom.uniform(1,N+1);
-                j = StdRandom.uniform(1,N+1);
-                if (!grid.isOpen(i,j)) {
-                    grid.open(i,j);
+            int count = 0;
+            while (!grid.percolates()) {
+                i = StdRandom.uniform(1, N+1);
+                j = StdRandom.uniform(1, N+1);
+                if (!grid.isOpen(i, j)) {
+                    grid.open(i, j);
                     count++;
                 }
             }
-            counts[a++] =(double) count/(N*N);
+            counts[a++] = (double) count/(N*N);
         }
     }
     // sample mean of percolation threshold
@@ -36,28 +35,27 @@ public class PercolationStats {
         return sum/T;
     }
     // sample standard deviation of percolation threshold
-    public double stddev(){
-        double ExpDev,SumExpDev=0.0;
+    public double stddev() {
+        double ExpDev, SumExpDev = 0.0;
         for (double z : counts) {
             ExpDev = (z - mean())*(z - mean());
-            SumExpDev+=ExpDev;
+            SumExpDev += ExpDev;
         }
-        return stddev = Math.sqrt( (SumExpDev)/(T-1) );
+        return Math.sqrt((SumExpDev)/(T-1));
     }
     // low  endpoint of 95% confidence interval
-    public double confidenceLo(){
-        return confidenceLo = mean() - (1.96*stddev())/(Math.sqrt(T));
+    public double confidenceLo() {
+        return mean() - (1.96*stddev())/(Math.sqrt(T));
     }
     // high endpoint of 95% confidence interval
-    public double confidenceHi(){
-        return confidenceHi = mean() + (1.96*stddev())/(Math.sqrt(T));
-
+    public double confidenceHi() {
+        return mean() + (1.96*stddev())/(Math.sqrt(T));
     }
 
     public static void main(String[] args) {
         int N = Integer.parseInt(args[0]);
         int T = Integer.parseInt(args[1]);
-        PercolationStats PS = new PercolationStats(N,T);
+        PercolationStats PS = new PercolationStats(N, T);
         StdOut.println("mean                             = " + PS.mean());
         StdOut.println("stddev                           = " + PS.stddev());
         StdOut.println("95% confidence interval          = " + PS.confidenceLo() + "," + PS.confidenceHi());
